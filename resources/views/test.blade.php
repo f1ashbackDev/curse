d6a1461630665f18bfce006af0e59761d33a1438
flashback <tapedappy@gmail.com>
2024-01-26 00:18:19 +0300

    add template admin.blade.php

---
+++ resources/views/test.blade.php
@@ -0,0 +1,4 @@
+@extends('pages.admin')
+@section('content')
+    <h1>Test</h1>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [UserController::class, 'indexPage'])->name('indexPage');
 Route::post('/register',[UserController::class,'register'])->name('register');
+Route::get('/admin', function (){
+    return view('test');
+});
